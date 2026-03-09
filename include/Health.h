97f4db5c6b395306062e855ea7da422dd7f29f56
Herdis Heida <herdisj24@ru.is>
2026-03-09 15:51:33 +0000

    health class basics

---
+++ include/Health.h
@@ -0,0 +1,34 @@
+#ifndef HEALTH_H
+#define HEALTH_H
+
+class Health {
+    private:
+        int current_;
+        int max_; // default 100
+
+    public:
+        explicit Health(int maxHp = 100) : current_(maxHp), max_(maxHp) {}
+
+        int current() const { return current_; }
+        int max() const { return max_; }
+
+        bool isFainted() const { return current_ <= 0; }
+
+        void damage(int amount) {
+            if (amount < 0) amount = 0;
+            current_ -= amount;
+            if (current_ < 0) current_ = 0;
+        }
+
+        void heal(int amount) {
+            if (amount < 0) amount = 0;
+            current_ += amount;
+            if (current_ > max_) current_ = max_;
+        }
+
+        void reset() {
+            current_ = max_;
+        }
+};
+
+#endif
